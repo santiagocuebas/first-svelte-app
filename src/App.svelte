94c1f546127d0eae82790069f57f082ce53fac82
Santiago Cuebas <sanlugus497@gmail.com>
2022-12-13 10:27:51 -0300

    First svelte app

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+	import Testimony from './components/Testimony.svelte';
+	
+	let data = [];
+
+	onMount(async () => {
+		const res = await fetch('data.json')
+			.then(res => res.json());
+
+		console.log(res);
+		data = res;
+	});
+</script>
+
+<div class='App'>
+  <div class='main-container'>
+    <h1 class='title-testimony'>
+			Here is what our alumni say about freeCodeCamp:
+		</h1>
+    {#each data as testimony}
+			<Testimony {...testimony} />
+		{/each}
+  </div>
+</div>
+
+<style>
+	:global(html, body) {
+		font-size: 18px;
+		background-color: #f5f6f7;
+	}
+
+	.main-container {
+		display: grid;
+		justify-content: center;
+		padding: 20px 0;
+		gap: 60px;
+	}
+
+	.title-testimony {
+		text-align: center;
+		margin: 0;
+		padding: 0;
+	}
+</style>
---
+++ src/components/Testimony.svelte
@@ -0,0 +1,60 @@
+<script>
+	export let image;
+	export let fullname;
+	export let country;
+	export let work;
+	export let company;
+	export let testimony;
+</script>
+
+<div class='container-testimony'>
+	<img
+		class='img-testimony'
+		src={`./images/testimony-${image}.png`}
+		alt={`Photo of ${image}`}
+	/>
+	<div class='container-text-testimony'>
+		<p class='name-testimony'><strong>{fullname}</strong> in {country}</p>
+		<p class='work-testimony'>{work} at <strong>{company}</strong></p>
+		<p class='content-testimony'>"{@html testimony}"</p>
+	</div>
+</div>
+
+<style>
+	.container-testimony {
+		display: flex;
+		align-items: center;
+		width: fit-content;
+		height: min-content;
+		background-color: #fff;
+		box-shadow: 0 3px 13px 1px #ccc;
+	}
+	
+	.img-testimony {
+		width: 350px;
+		height: 350px;
+	}
+	
+	.container-text-testimony {
+		min-width: 550px;
+		width: 100%;
+		max-width: 750px;
+		height: 350px;
+		text-align: center;
+		padding: 40px;
+		line-height: 1.5rem;
+		font-family: Lato, sans-serif;
+		font-weight: 400;
+		color: #444;
+	}
+	
+	.name-testimony, .work-testimony, .content-testimony {
+		font-size: 1.3rem;
+		margin: 15px;
+	}
+	
+	.content-testimony {
+		font-size: 1.1rem;
+		text-align: justify;
+	}
+</style>
